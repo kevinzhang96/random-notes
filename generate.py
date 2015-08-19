09bed108c4012ff51be53dfdd6e28c07fe58c398
Kevin <kevin@genmobapp.com>
2015-08-18 23:40:39 -0700

    wav generation fixed

--- generate.py
+++ generate.py
@@ -2,41 +2,59 @@
 
 import sys
 import os
+import random
 import struct
+import numpy
 import re
 import wave
 
-if len(sys.argv) != 1:
-    print 'Please provide the correct parameters: number of seconds.'
+if len(sys.argv) != 3:
+    print 'Please provide the correct parameters: number of seconds and notes per second.'
     sys.exit(0)
 
-WAVLENGTH = int(sys.argv[0])
-# NOTERATE  = int(sys.argv[1])
+FPS = 44100 # the original number of frames per second in a wave file
+
+WAVSECONDS = int(sys.argv[1])
+WAVLENGTH  = WAVSECONDS * FPS
+NOTERATE   = int(sys.argv[2])
 
 CURRENTDIR = os.getcwd()
-WAVEDIR    = CURRENTDIR + '/wavs/'
-DIRFILES   = [filename for filename in os.listdir(WAVEDIR) if os.path.isfile(WAVEDIR + filename)]
+WAVDIR    = CURRENTDIR + '/wavs/'
+DIRFILES   = [filename for filename in os.listdir(WAVDIR) if os.path.isfile(WAVDIR + filename)]
 WAVPATTERN = re.compile('^randomwav\d{3}.wav')
 WAVFILES   = [file for file in DIRFILES if WAVPATTERN.match(file)]
 WAVFILES.sort()
 WAVFILECOUNT = len(WAVFILES)
 
+FREQARRAY = [130.8, 138.6, 146.8, 155.6, 164.8, 174.6, 185.0, 196.0, 207.7, 220.0, 233.1, 246.9, 261.6]
+
 if WAVFILECOUNT > 1000:
     print 'Please clear your /wavs directory before continuing; too many random files have been generated.'
 else:
     NewNumber = str(WAVFILECOUNT).zfill(3)
-    FileName  = 'randomwav' + NewNumber + '.wav'
+    FileName  = WAVDIR + 'randomwav' + NewNumber + '.wav'
     output = wave.open(FileName, 'w')
     output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
 
-    values = []
+    value_str = ''
 
-    for i in range(0, WAVLENGTH):
-        value = random.randint(-32767, 32767)
+    for i in range(0, WAVSECONDS * NOTERATE):
+        value = FREQARRAY[random.randint(0, len(FREQARRAY) - 1)]
         packed_value = struct.pack('h', value)
-        values.append(packed_value)
-        values.append(packed_value)
 
-    value_str = ''.join(values)
+        print 'Writing value ' + str(value)
+        period = float(FPS / NOTERATE) / float(value)
+        omega = numpy.pi * 2 / period
+        xaxis = numpy.arange(int(period), dtype = numpy.float) * omega
+        ydata = 16384 * numpy.sin(xaxis)
+
+        signal = numpy.resize(ydata, (WAVLENGTH / NOTERATE,))
+
+        ssignal = ''
+
+        for j in range(len(signal)):
+            ssignal += struct.pack('h', signal[j])
+        value_str += ssignal
+
     output.writeframes(value_str)
     output.close()
